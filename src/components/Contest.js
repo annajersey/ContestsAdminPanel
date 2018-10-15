757a37100106f828a3e18a8862e880f77bf4971c
anna <anna.bogomiagkova@codeit.com.ua>
2018-10-15 18:04:23 +0300

    starting with grid

--- src/components/Contest.js
+++ src/components/Contest.js
@@ -1,5 +1,32 @@
 import React, {Component} from "react";
+import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
+const SortableItem = SortableElement(({value}) =>
+    <li>{value}</li>
+);
 
+const SortableList = SortableContainer(({items}) => {
+    return (
+        <ul>
+            {items.map((value, index) => (
+                <SortableItem key={`item-${index}`} index={index} value={value} />
+            ))}
+        </ul>
+    );
+});
+
+class SortableComponent extends Component {
+    state = {
+        items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+    };
+    onSortEnd = ({oldIndex, newIndex}) => {
+        this.setState({
+            items: arrayMove(this.state.items, oldIndex, newIndex),
+        });
+    };
+    render() {
+        return <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />;
+    }
+}
 class Contest extends Component {
     constructor(props) {
         super(props);
@@ -11,8 +38,10 @@ class Contest extends Component {
     render() {
         return (
             <div>
+               <SortableComponent />
                 <div className="header">Contest <h3>ID: {this.props.match.params.id}</h3></div>
                 <div className="contest">
+
                     <div className="switcher">
                         <button className={this.state.page === "l" ? "active" : ""}
                             onClick={() => this.setState({page: "l"})}>Leaderboard
@@ -23,8 +52,8 @@ class Contest extends Component {
                     </div>
                     <div className="contestContent">
                         {this.state.page === "l" && <div className="leaderBoard">
-                            {[0,1,2,3,4,5,6,7,8,9].map(i=> <div className="member">
-                                <div className="photo"><div className="number">5</div><img src={require("../assets/images/photo.png")}  /></div>
+                            {[1,2,3,4,5,6].map(i=> <div className="member">
+                                <div className="photo"><div className="number">5</div><img src={require("../assets/images/members/"+i+".png")}  /></div>
                             </div>)}
 
                         </div>}
