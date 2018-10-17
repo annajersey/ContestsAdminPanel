a492c53249c3dabffe623a27033c6780b923e7fd
anna <anna.bogomiagkova@codeit.com.ua>
2018-10-17 14:54:01 +0300

    contests list continue

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Sortable from 'sortablejs'
-
+import Facebook from '../assets/images/fbicon.svg';
+import Instagram from '../assets/images/instaicon.svg';
 class Leaderboard extends Component {
     componentDidMount() {
         var el = this.list;
@@ -10,12 +11,16 @@ class Leaderboard extends Component {
     render() {
         return (
             <div className="boardItems" ref={(e) => this.list = e}>
-                {[1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6].map(i =>
+                {[1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6, 1, 2, 3].map(i =>
                     <div className="member">
                         <div className="photo">
                             <div className="number">5</div>
-                            <img src={require("../assets/images/members/" + i + ".png")}/></div>
-                    </div>)}
+                            <img src={require("../assets/images/members/" + i + ".png")}/>
+                        </div>
+                        <div className="userName">Teresa<br />Norris</div>
+                        <div className="socials"><Facebook width="22" height="22" /><Instagram width="22"  height="22"/></div>
+                    </div>
+                )}
             </div>
         );
     }
