00ea1baf11a43a9d10e9df0f9ba21d9d852c99ef
anna <anna.bogomiagkova@codeit.com.ua>
2018-10-25 17:59:46 +0300

    get images

--- src/components/ContestsList.js
+++ src/components/ContestsList.js
@@ -1,80 +1,39 @@
 import React from "react";
 import Timer from "./Timer";
 import axios from "axios";
+import {ImageLoader} from "react-image-file";
 
 class ContestsList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            contests: []
+            contests: [],
+            images: {}
         };
+        const AuthStr = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbGV4YW5kZXIua3Vwcmlrb3ZAY29kZWl0LmNvbS51YSIsImV4cCI6MTU0MTMzMjg3N30.DMnrycHPWrnh5p1-407PG55c-LmZxz8UWZPVFhHeiXWpYR-ltYj5U53tvFPp6YMVazXiGHT9KpKGblk2tNU8YQ";
+
+        axios.defaults.headers.common["Authorization"] = AuthStr;
     }
 
     componentDidMount() {
-        // fetch("http://b.dcodeit.net:8080/smartpay/contest", {
-        //     method: "GET",
-        //     headers: {
-        //         "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbGV4YW5kZXIua3Vwcmlrb3ZAY29kZWl0LmNvbS51YSIsImV4cCI6MTU0MTI0ODA2M30.a8kV5un6enpx3fqMDEcpsMNdQmO6MXt7IH3reOVjuz9H1Tcl-TxdEFtow_nkRx21cSLrjpmqoXIzep5ITgF8rQ"
-        //     }
-        // })
-        //     .then(response => {
-        //         if (!response.ok) {
-        //             throw Error("Network request failed");
-        //         }
-        //         return response;
-        //     })
-        //     .then(d => d.json())
-        //     .then(result => {
-        //         console.log(result);
-        //     }, () => {
-        //
-        //         console.log("Request Failed");
-        //
-        //     });
-        // axios.defaults.headers.common['Authorization'] = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbGV4YW5kZXIua3Vwcmlrb3ZAY29kZWl0LmNvbS51YSIsImV4cCI6MTU0MTI0ODA2M30.a8kV5un6enpx3fqMDEcpsMNdQmO6MXt7IH3reOVjuz9H1Tcl-TxdEFtow_nkRx21cSLrjpmqoXIzep5ITgF8rQ"
-        // ;
-         let url = "http://b.dcodeit.net:8080/smartpay/contest";
- let AuthStr = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbGV4YW5kZXIua3Vwcmlrb3ZAY29kZWl0LmNvbS51YSIsImV4cCI6MTU0MDM4MzY1N30.TmNoWJZMV_LbWi-y8NlkjezVVWiIoQZe9RBTbKSrKiqpo7p_htCjWGqjIiGv5ICpDKS5i-jvGbu3fahSNzkL7A"
-
-        // axios.get("http://b.dcodeit.net:8080/smartpay/contest",
-        //     { headers: {
-        //             "Content-Type": "application/x-www-form-urlencoded",
-        //         Authorization: "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbGV4YW5kZXIua3Vwcmlrb3ZAY29kZWl0LmNvbS51YSIsImV4cCI6MTU0MTI0ODA2M30.a8kV5un6enpx3fqMDEcpsMNdQmO6MXt7IH3reOVjuz9H1Tcl-TxdEFtow_nkRx21cSLrjpmqoXIzep5ITgF8rQ"
-        //
-        //     },withCredentials: true }
-        // )
-        axios({ method: 'get', url: url, headers: { 'Authorization': AuthStr } })
-        // axios({
-        //     method: "get",
-        //     url: "http://b.dcodeit.net:8080/smartpay/contest",
-        //
-        //     config: {
-        //        // withCredentials: true,
-        //         headers: {
-        //            // "Content-Type": "application/x-www-form-urlencoded",
-        //             "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbGV4YW5kZXIua3Vwcmlrb3ZAY29kZWl0LmNvbS51YSIsImV4cCI6MTU0MDM4MzY1N30.TmNoWJZMV_LbWi-y8NlkjezVVWiIoQZe9RBTbKSrKiqpo7p_htCjWGqjIiGv5ICpDKS5i-jvGbu3fahSNzkL7A"
-        //
-        //         }
-        //     }
-        // })
-            .then(res => {
-                //this.setState({contests: res.data});
-                console.log(res.data);
-            })
-            .catch(e=>console.log(e))
+        axios.get("http://b.dcodeit.net:8080/smartpay/contest").then(res => {
+            const contests = res.data;
+            contests.forEach(c => {
+                axios.get(`http://b.dcodeit.net:8080/smartpay/image/${c.imageId}`,  {
+                    headers: {"Content-Type": "image/png", "Charset":"UTF-8"}
+                }).then(res => {
+                    //window.atob
+                    //this.i.innerHTML=btoa(unescape(encodeURIComponent(res.data)))
+                    this.setState({images:{...this.state.images, [c.imageId]:res.data}})
+                });
+            });
+            this.setState({contests: res.data});
+            console.log(res.data);
+        })
+            .catch(e => console.log(e))
         ;
-//
-//         axios.get(url, { headers: { Authorization: AuthStr } })
-//             .then(response => {
-//                 // If request is good...
-//                 console.log(response.data);
-//             })
-//             .catch((error) => {
-//                 console.log('error ' + error);
-//             });
 
-
-     }
+    }
 
     onContestClick(id) {
         console.log("/contest/" + id);
@@ -82,15 +41,19 @@ class ContestsList extends React.Component {
     }
 
     render() {
-
+console.log(this.state.images)
         return (
             <div className="constestList">
+                <div ref={i=>this.i=i}></div>
                 {this.state.contests.map(contest => {
                     return <div key={contest.id} className="constestBlock">
                         {/*<div style={{'background-image':'url("http://b.dcodeit.net:8080/smartpay/image/'+contest.imageId+'")'}}*/}
                         {/*className="topImage2" onClick={() => this.onContestClick(contest.id)}></div>*/}
                         <img className="topImage" onClick={() => this.onContestClick(contest.id)}
-                             src={`http://b.dcodeit.net:8080/smartpay/image/${contest.imageId}`}/>
+                             src={`data:image/png;base64,${this.state.images[contest.imageId]}`}
+
+                            />
+                        <ImageLoader file={this.state.images[contest.imageId]} alt='some text'/>
                         <div className="shortInfo">
                             <h2 onClick={() => this.onContestClick(i)}>{contest.name}</h2>
                             <div className="briefDesc">{contest.description.substring(0, 35)}</div>
@@ -101,18 +64,7 @@ class ContestsList extends React.Component {
                         </div>
                     </div>;
                 })}
-                {/*{[1, 2, 3].map(i => {*/}
-                {/*return <div key={i} className="constestBlock">*/}
-                {/*<img className="topImage" onClick={() => this.onContestClick(i)}*/}
-                {/*src={require("../assets/images/contests/" + i + ".png")}/>*/}
-                {/*<div className="shortInfo">*/}
-                {/*<h2 onClick={() => this.onContestClick(i)}>Bershka</h2>*/}
-                {/*<div className="briefDesc">One of the best fashion shop</div>*/}
-                {/*<div className="info"><span>12 Giveaway</span><span className="timer"><img*/}
-                {/*src={require("../assets/images/timer.png")}/>36:32:25</span></div>*/}
-                {/*</div>*/}
-                {/*</div>;*/}
-                {/*})}*/}
+
             </div>
         );
     }
